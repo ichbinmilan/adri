1d2f6a80a2efd948d9c5fb41d3e367853820b1f4
milan <milan@neonett.net>
2019-05-06 00:17:53 +0300

    all without contact form

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StaticController extends AbstractController
 {
+    private $message = [];
+
     /**
      * @Route("/", name="home")
      */
@@ -26,8 +29,37 @@ class StaticController extends AbstractController
     /**
      * @Route("/contacts", name="contacts")
      */
-    public function contacts()
+    public function contacts(Request $request)
     {
+        if (!empty($request->getContent())) {
+            $messageArr['name'] = strip_tags($request->get('name'));
+            $messageArr['email'] = strip_tags($request->get('email'));
+            $messageArr['subject'] = strip_tags($request->get('subject'));
+            $messageArr['message'] = strip_tags($request->get('message'));
+
+//            $this->message = $messageArr;
+//            $this->sendmail(new \Swift_Mailer('gmail'));
+
+//            dd($name, $email, $subject, $message);
+            $this->addFlash('success', 'The message was NOT sent successfully. Please send me e-mail');
+//            $this->addFlash('success', 'The message was sent successfully. Thank You!');
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('contacts.html.twig');
     }
+
+    function sendmail(\Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message())
+            ->setSubject('Message from Adri\'s light site')
+            ->setFrom($this->message['email'])
+            ->setTo(getenv('mymail'))
+            ->setBody($this->renderView('email.html.twig',
+                ['message' => $this->message,]
+            ), 'text/html');
+        $mailer->send($message);
+    }
+
 }
